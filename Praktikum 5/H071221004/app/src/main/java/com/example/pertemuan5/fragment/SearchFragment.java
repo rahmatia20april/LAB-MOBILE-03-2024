a47e65797084a2af1462b84ef441bbeae9da735a
rahmatia20april <rahmatiaa737@gmail.com>
2024-04-26 22:54:52 +0800

    kumpul tugas 5

---
+++ Praktikum 5/H071221004/app/src/main/java/com/example/pertemuan5/fragment/SearchFragment.java
@@ -0,0 +1,98 @@
+package com.example.pertemuan5.fragment;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.pertemuan5.DataSource;
+import com.example.pertemuan5.Instagram;
+import com.example.pertemuan5.R;
+import com.example.pertemuan5.SeacrhAdapter;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SearchFragment extends Fragment {
+
+    private ArrayList<Instagram> instagramList;
+    private RecyclerView recyclerView;
+    private SeacrhAdapter adapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        instagramList = new ArrayList<>();
+
+        adapter = new SeacrhAdapter(instagramList);
+        recyclerView.setAdapter(adapter);
+
+        ProgressBar progressBar = view.findViewById(R.id.progressBar);
+
+        androidx.appcompat.widget.SearchView searchView = view.findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new androidx.appcompat.widget.SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                progressBar.setVisibility(View.VISIBLE);
+
+                ExecutorService executor = Executors.newSingleThreadExecutor();
+                Handler handler = new Handler(Looper.getMainLooper());
+                executor.execute(() -> {
+                    ArrayList<Instagram> filteredList = new ArrayList<>();
+                    if (!newText.isEmpty()) {
+                        for (Instagram item : DataSource.instagrams) {
+                            if (item.getUsername().toLowerCase().contains(newText.toLowerCase()) ||
+                                    item.getName().toLowerCase().contains(newText.toLowerCase())) {
+                                filteredList.add(item);
+                            }
+                        }
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    handler.post(() -> {
+                        progressBar.setVisibility(View.GONE);
+                        instagramList.clear();
+                        if (!newText.isEmpty()) {
+                            instagramList.addAll(filteredList);
+                        }
+                        adapter.notifyDataSetChanged();
+                    });
+                });
+                return true;
+            }
+        });
+
+    }
+
+}
