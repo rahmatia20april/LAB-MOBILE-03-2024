02af4aaac9ebbe57f4a2bf0f50ad79f95262a03b
FailFudhayl <ahmadfudhayl06@gmail.com>
2024-04-26 13:17:40 +0800

    kumpul tugas 5

---
+++ Praktikum 5/H071221026/app/src/main/java/com/example/praktikum5/HomeFragment.java
@@ -0,0 +1,45 @@
+package com.example.praktikum5;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class HomeFragment extends Fragment {
+
+    public static final String EXTRA_INSTAGRAM = "extra_instagram";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView rvPostingan = view.findViewById(R.id.rv_post);
+        rvPostingan.setHasFixedSize(true);
+        PostinganAdapter postinganAdapter = new PostinganAdapter(DataSource.instagrams);
+        rvPostingan.setAdapter(postinganAdapter);
+
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            Instagram instagram = bundle.getParcelable(EXTRA_INSTAGRAM);
+            if (instagram != null) {
+                DataSource.instagrams.add(0, instagram);
+                postinganAdapter.notifyDataSetChanged();
+            }
+        }
+    }
+}
